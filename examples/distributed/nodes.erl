b0c44de678235115714dfa1581ac2e83117609c5
gfabbretti8 <g.fabbretti8@gmail.com>
2020-09-17 12:56:31 +0200

    update example

--- examples/distributed/nodes.erl
+++ examples/distributed/nodes.erl
@@ -2,18 +2,11 @@
 -export([start/0, child/1]).
 
 
-
-
 start() ->
   Child = spawn(?MODULE, child, [self()]),
-  self() ! checkpoint,
   U = nodes(),
-  Child ! vai1,
   U.
 
 
-
-
 child(_P) ->
-  receive vai1 -> ok end,
   slave:start('mac', 'coolName').
